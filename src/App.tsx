dbe31f248523769ba562f80fec70ae8b7f00c033
C_elare <houvillev@gmail.com>
2024-01-08 19:32:56 +0100

    feat: Update import paths in App.tsx and fix CSS styling (main)

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
 import {FC} from 'react';
 import './App.css';
-import { useThemeStore } from "./store/themeStore.tsx";
 import {Header} from "./components/header/Header.tsx";
-import {Home} from "./components/home/Home.tsx";
+import {Home} from "./components/startmenu/Home.tsx";
+import {BackGroundIcon} from "./components/startmenu/BackGroundIcon.tsx";
 
 export const App: FC = () => {
-    const {theme, textColor} = useThemeStore();
-
-    const containerStyle = {
-        backgroundColor: theme,
-        color: textColor
-    };
-
-
-
 
     return (
-        <div className="full-size-container" style={containerStyle}>
-            <Header/>
-            <Home />
-        </div>
+            <BackGroundIcon>
+                <Header/>
+                <Home />
+            </BackGroundIcon>
     );
 }
