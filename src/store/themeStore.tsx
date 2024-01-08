c1b54fc5edf2f3304c833fde970892f39a64866f
C_elare <houvillev@gmail.com>
2024-01-08 19:34:57 +0100

    feat(themeStore): Add dark mode icons and background images (main)

--- src/store/themeStore.tsx
+++ src/store/themeStore.tsx
@@ -1,27 +1,74 @@
 import {create, StoreApi, UseBoundStore} from "zustand";
+import mobileIconLight from './../assets/images/pattern-background-mobile-light.svg';
+import mobileIconDark from './../assets/images/pattern-background-mobile-dark.svg';
+import tabletIconLight from './../assets/images/pattern-background-tablet-light.svg';
+import tabletIconDark from './../assets/images/pattern-background-tablet-dark.svg';
+import desktopIconLight from './../assets/images/pattern-background-desktop-light.svg';
+import desktopIconDark from './../assets/images/pattern-background-desktop-dark.svg';
+import moonLight from './../assets/images/icon-moon-light.svg';
+import moonDark from './../assets/images/icon-moon-dark.svg';
+import sunLight from './../assets/images/icon-sun-light.svg'
+import sunDark from './../assets/images/icon-sun-dark.svg';
 
-enum BgColor {
+export enum BgColor {
     light= '#F4F6FA',
     black = '#313E51'
 }
-
-enum TextColor {
+export enum BgButton {
+    light = '#FFF',
+    black = '#3B4D66'
+}
+export enum TextColor {
     light = '#FFFFFF',
     black = '#313E51'
 }
-
+export enum SubTextColor {
+    light = '#ABC1E1',
+    black = '#626C7F'
+}
 
 type StoreTheme = {
-    theme: BgColor,
+    backgroundColor: BgColor,
+    themeButton: BgButton,
     textColor: TextColor,
+    subTextColor: SubTextColor,
+    sunIcon: { light: string; dark: string;},
+    moonIcon: {light: string; dark:string;},
+    mobileIcon: { light:string; dark: string;},
+    tabletIcon: { light: string; dark: string; },
+    desktopIcon: { light: string; dark: string; },
     toggleChangeTheme: () => void
 }
 
 export const useThemeStore:  UseBoundStore<StoreApi<StoreTheme>> = create<StoreTheme>()((set) =>({
-        theme: BgColor.light,
+        backgroundColor: BgColor.light,
+        themeButton: BgButton.light,
         textColor: TextColor.black,
+        subTextColor: SubTextColor.black,
+        sunIcon: {
+            light: sunLight,
+            dark: sunDark
+        },
+        moonIcon: {
+            light: moonLight,
+            dark: moonDark,
+        },
+        mobileIcon: {
+            light: mobileIconLight,
+            dark: mobileIconDark,
+        },
+        tabletIcon: {
+            light: tabletIconLight,
+            dark: tabletIconDark
+        },
+        desktopIcon: {
+            light: desktopIconLight,
+            dark: desktopIconDark
+        },
         toggleChangeTheme: () => set((state) => ({
-            theme: state.theme === BgColor.light ? BgColor.black : BgColor.light,
+            backgroundColor: state.backgroundColor === BgColor.light ? BgColor.black : BgColor.light,
+            themeButton: state.themeButton === BgButton.light ? BgButton.black : BgButton.light,
             textColor: state.textColor === TextColor.black ? TextColor.light : TextColor.black,
+            subTextColor: state.subTextColor ===  SubTextColor.black? SubTextColor.light : SubTextColor.black,
         })),
 }))
