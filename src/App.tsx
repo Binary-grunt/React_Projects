ed1c2b7219762bb252e1450f7a6e3060b3754fab
C_elare <houvillev@gmail.com>
2024-01-08 19:48:42 +0100

    feat(App): update imports in App.tsx (main)

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import {FC} from 'react';
 import './App.css';
-import {Header} from "./components/header/Header.tsx";
-import {StartMenuPage} from "./components/startmenu/StartMenuPage.tsx";
-import {BackGroundIcon} from "./components/startmenu/BackGroundIcon.tsx";
+import {Header} from "./components/Header/Header.tsx";
+import {StartMenuPage} from "./pages/StartMenuPage.tsx";
+import {BackGroundIcon} from "./themes/BackGroundIcon.tsx";
 
 export const App: FC = () => {
 
