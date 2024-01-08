4b4c65ea77a0784bd38e71ab1eb81cf63b7bd2ea
C_elare <houvillev@gmail.com>
2024-01-08 19:35:25 +0100

    feat(startmenu): add Title component
    - Add Title component to display a title with bold content
    - Use the useThemeStore hook to get the subTextColor
    - Style the component with flexbox and appropriate font styles
    
    Issue: main

---
+++ src/components/startmenu/Title.tsx
@@ -0,0 +1,23 @@
+import {FC} from "react";
+import {useThemeStore} from "../../store/themeStore.tsx";
+
+type Title = {
+    content: string,
+    contentBold: string
+}
+export const Title:FC<Title>= ({content, contentBold}) => {
+    const {subTextColor} = useThemeStore();
+    return (
+        <>
+            <div className={'flex flex-col font-rubik font-light gap-3 m-7'}>
+                <div className={'text-5xl'}> <h1>{content}</h1>
+                    <div className={'font-rubik font-normal'}>
+                    {contentBold}
+                    </div>
+                </div>
+                <p style={{color: subTextColor}} className={'font-rubik italic'}>Pick a subject to get started</p>
+            </div>
+
+        </>
+    );
+};
