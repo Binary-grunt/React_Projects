fe68c498fecc00130657f3f22852832de8bbcdfa
C_elare <houvillev@gmail.com>
2024-01-08 19:34:30 +0100

    feat(App): update import statement for StartMenuPage component (main)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import {FC} from 'react';
 import './App.css';
 import {Header} from "./components/header/Header.tsx";
-import {Home} from "./components/startmenu/Home.tsx";
+import {StartMenuPage} from "./components/startmenu/StartMenuPage.tsx";
 import {BackGroundIcon} from "./components/startmenu/BackGroundIcon.tsx";
 
 export const App: FC = () => {
@@ -9,7 +9,7 @@ export const App: FC = () => {
     return (
             <BackGroundIcon>
                 <Header/>
-                <Home />
+                <StartMenuPage />
             </BackGroundIcon>
     );
 }
