7b9baf42be50dfad09d9a9683bea0d85b0f29d06
C_elare <houvillev@gmail.com>
2024-01-09 06:06:44 +0100

    feat(QuizPage): Add functionality to handle quiz questions and answers
    - Add state variables for currentQuestionIndex, select, and isAnswered
    - Implement handleChange function to update select state when an option is selected
    - Implement handleSubmitAnswer function to handle form submission and check if the selected answer is correct
    - Implement handleNextQuestion function to move to the next question if the current question is answered
    - Update JSX to display quiz header, question, progress bar, options, and submit button
    - Add loading message if quizzes are not yet loaded
    
    Issue: main

--- src/pages/QuizPage.tsx
+++ src/pages/QuizPage.tsx
@@ -1,11 +1,12 @@
-import {FC, useState} from 'react';
+import {ChangeEvent, FC, FormEvent, useState} from 'react';
 import { useDataStore } from '../store/dataStore';
-import {ButtonSubject} from "../components/Button/ButtonSubject.tsx";
+import {ButtonQuiz} from "../components/Button/ButtonQuiz.tsx";
 import {Header} from "../components/Header/Header.tsx";
 import {useThemeStore} from "../store/themeStore.tsx";
 import {Subject} from "../components/Header/Subject.tsx";
 import {ProgressBar} from "../components/Quiz/ProgressBar.tsx";
 import {QuestionQuiz} from "../components/Quiz/QuestionQuiz.tsx";
+import {ButtonSubmit} from "../components/Button/ButtonSubmit.tsx";
 
 type QuizPageProps = {
     index?: number,
@@ -13,53 +14,77 @@ type QuizPageProps = {
 
 export const QuizPage:FC<QuizPageProps> = ({index = 1}) => {
     const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+    const [select, setSelect] = useState<string | null>(null);
+    const [isAnswered, setIsAnswered] = useState(false);
+
     const { quizzes } = useDataStore();
     const {subTextColor} = useThemeStore();
-    const backgroundColorIcon = ['#FFF1E9', '#E0FDEF','#EBF0FF','#F6E7FF'];
 
-    if (quizzes.length === 0) return <div>Loading...</div>;
+    const backgroundColorIcon: string[] = ['#FFF1E9', '#E0FDEF','#EBF0FF','#F6E7FF'];
 
     const quiz = quizzes[index];
     const question = quiz.questions[currentQuestionIndex];
 
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setSelect(e.target.value)
+    }
+    const handleSubmitAnswer = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        if (select === undefined || select === null) {
+            console.log('No answer selected');
+            return;
+        }
+        if (select === question.answer) {
+            console.log('Good');
+        } else {
+            console.log('Not good');
+        }
+
+        console.log(`select: ${select}`);
+        console.log(`answer: ${question.answer}`);
+        setIsAnswered(true);
+    };
+
     const handleNextQuestion = () => {
-        if (currentQuestionIndex < quiz.questions.length - 1) {
+        if (!isAnswered) {
+            return;
+        } else if (currentQuestionIndex < quiz.questions.length - 1) {
             setCurrentQuestionIndex(currentQuestionIndex + 1);
         }
     };
 
     return (
         <>
-            <div className={'flex-col flex justify-center '}>
-            <Header>
-                <Subject
-                    icon={quiz.icon}
-                    title={quiz.title}
-                    backgroundColor={backgroundColorIcon[index % backgroundColorIcon.length]}
-                />
-            </Header>
-            <div className={'flex flex-col justify-items-center mx-6'}>
-                <QuestionQuiz
-                    subTextColor={subTextColor}
-                    currentIndexQuestion={currentQuestionIndex}
-                    quizQuestionLength={quiz.questions.length}
-                    quizQuestion={question.question}
-                />
-                <ProgressBar progress={currentQuestionIndex}/>
-                <div className={'flex flex-col pt-8'}>
-                    <ButtonSubject
-                        backgroundColor={'#F4F6FA'}
-                        options={question.options}
-                    />
-                    <button
-                        style={{color: '#FFF'}}
-                        className={'bg-violet-600 rounded-xl  my-2 p-3 font-rubik text-lg'}
-                        onClick={handleNextQuestion}>
-                        Submit Answer
-                    </button>
+            {quizzes.length > 0 &&
+                <div className={'flex-col flex justify-center '}>
+                    <Header>
+                        <Subject
+                            icon={quiz.icon}
+                            title={quiz.title}
+                            backgroundColor={backgroundColorIcon[index % backgroundColorIcon.length]}
+                        />
+                    </Header>
+                    <div className={'flex flex-col justify-items-center mx-6 pt-14'}>
+                        <QuestionQuiz
+                            subTextColor={subTextColor}
+                            currentIndexQuestion={currentQuestionIndex}
+                            quizQuestionLength={quiz.questions.length}
+                            quizQuestion={question.question}
+                        />
+                        <ProgressBar progress={currentQuestionIndex}/>
+                        <form onSubmit={handleSubmitAnswer} className={'flex flex-col pt-8'}>
+                            <ButtonQuiz
+                                backgroundColor={'#F4F6FA'}
+                                options={question.options}
+                                onChangeClick={handleChange}
+                            />
+                            <ButtonSubmit onSubmitClick={handleNextQuestion}/>
+                        </form>
+                    </div>
                 </div>
-            </div>
-            </div>
+            }
+            {!quizzes.length &&
+            <div>Loading...</div>}
         </>
     );
 };
