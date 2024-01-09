ec12f6606c41b8aee5510ae6b97d2168e019f3e2
C_elare <houvillev@gmail.com>
2024-01-09 03:28:10 +0100

    feat(Header): Add background color to Subject component (main)

--- src/components/Header/Subject.tsx
+++ src/components/Header/Subject.tsx
@@ -3,13 +3,14 @@ import {FC} from "react";
 type SubjectTitleType = {
     icon: string,
     title: string
+    backgroundColor: string,
 }
-export const Subject: FC <SubjectTitleType> = ({icon, title}) => {
+export const Subject: FC <SubjectTitleType> = ({icon, title, backgroundColor}) => {
     return (
         <>
-            <div className={'flex flex-row'}>
-                <img src={icon} alt={title}/>
-                <h2>{title}</h2>
+            <div className={'flex flex-row content-center font-rubik font-normal items-center'}>
+                <img className={'rounded-lg p-2 mr-4 size-12'} src={icon} alt={title} style={{backgroundColor }}/>
+                <h2 className={'text-xl'}>{title}</h2>
             </div>
 
         </>
