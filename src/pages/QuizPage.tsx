c22ddf489563f192d83b462c993482189dbb7ab2
C_elare <houvillev@gmail.com>
2024-01-09 04:12:33 +0100

    feat(QuizPage): Update imports and component structure
    - Update import paths for ButtonSubject, Header, Subject, ProgressBar, and QuestionQuiz components
    - Update component structure to include QuestionQuiz and ProgressBar components
    - Update styling and class names for improved readability and consistency
    
    Issue: main

--- src/pages/QuizPage.tsx
+++ src/pages/QuizPage.tsx
@@ -1,15 +1,17 @@
 import {FC, useState} from 'react';
 import { useDataStore } from '../store/dataStore';
-import {ButtonSubject} from "../components/Quiz/Buttons/ButtonSubject.tsx";
+import {ButtonSubject} from "../components/Button/ButtonSubject.tsx";
 import {Header} from "../components/Header/Header.tsx";
 import {useThemeStore} from "../store/themeStore.tsx";
 import {Subject} from "../components/Header/Subject.tsx";
+import {ProgressBar} from "../components/Quiz/ProgressBar.tsx";
+import {QuestionQuiz} from "../components/Quiz/QuestionQuiz.tsx";
 
 type QuizPageProps = {
     index?: number,
 }
 
-export const QuizPage:FC<QuizPageProps> = ({index = 3}) => {
+export const QuizPage:FC<QuizPageProps> = ({index = 1}) => {
     const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
     const { quizzes } = useDataStore();
     const {subTextColor} = useThemeStore();
@@ -28,6 +30,7 @@ export const QuizPage:FC<QuizPageProps> = ({index = 3}) => {
 
     return (
         <>
+            <div className={'flex-col flex justify-center '}>
             <Header>
                 <Subject
                     icon={quiz.icon}
@@ -35,27 +38,28 @@ export const QuizPage:FC<QuizPageProps> = ({index = 3}) => {
                     backgroundColor={backgroundColorIcon[index % backgroundColorIcon.length]}
                 />
             </Header>
-
-                <div className={'flex flex-col p-3'}>
-                    <div className={'pl-5'}>
-                        <p style={{color: subTextColor}} className={'font-rubik italic font-light'}>
-                            Question {currentQuestionIndex + 1} of {quiz.questions.length}
-                        </p>
-                        <h2 className={'text-2xl font-rubik'}>{question.question}</h2>
-                    </div>
-                    <div className={'flex flex-col'}>
-                        <ButtonSubject
-                            backgroundColor={'#F4F6FA'}
-                            options={question.options}
-                        />
-                        <button
-                            style={{backgroundColor: '#A729F5', color: '#FFF'}}
-                            className={'rounded-xl mx-6 my-2 p-3 font-rubik text-lg pl-2'}
-                            onClick={handleNextQuestion}>
-                            Submit Answer
-                        </button>
-                    </div>
+            <div className={'flex flex-col justify-items-center mx-6'}>
+                <QuestionQuiz
+                    subTextColor={subTextColor}
+                    currentIndexQuestion={currentQuestionIndex}
+                    quizQuestionLength={quiz.questions.length}
+                    quizQuestion={question.question}
+                />
+                <ProgressBar progress={currentQuestionIndex}/>
+                <div className={'flex flex-col pt-8'}>
+                    <ButtonSubject
+                        backgroundColor={'#F4F6FA'}
+                        options={question.options}
+                    />
+                    <button
+                        style={{color: '#FFF'}}
+                        className={'bg-violet-600 rounded-xl  my-2 p-3 font-rubik text-lg'}
+                        onClick={handleNextQuestion}>
+                        Submit Answer
+                    </button>
                 </div>
+            </div>
+            </div>
         </>
     );
 };
