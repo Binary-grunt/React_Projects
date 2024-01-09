921bd22b7a4ee9cb8bdd3c419c1b069bded0d3e4
C_elare <houvillev@gmail.com>
2024-01-09 03:29:01 +0100

    feat(App): Add QuizPage component (main)

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import {FC} from 'react';
 import './App.css';
-import {Header} from "./components/Header/Header.tsx";
 import {StartMenuPage} from "./pages/StartMenuPage.tsx";
 import {BackGroundIcon} from "./themes/BackGroundIcon.tsx";
+import {QuizPage} from "./pages/QuizPage.tsx";
 
 export const App: FC = () => {
 
     return (
             <BackGroundIcon>
-                <Header/>
-                <StartMenuPage />
+                <QuizPage/>
+                {/*<StartMenuPage />*/}
             </BackGroundIcon>
     );
 }
