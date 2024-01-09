3ddb40c0e2ba368dbf958068aca1601ae6fe27a7
C_elare <houvillev@gmail.com>
2024-01-09 06:06:49 +0100

    feat(StartMenuPage): Add start menu page layout and button subjects (main)

--- src/pages/StartMenuPage.tsx
+++ src/pages/StartMenuPage.tsx
@@ -8,35 +8,25 @@ enum StartTitle {
     content = "Welcome to the",
     contentBold = "Frontend Quiz!"
 }
-enum EndTitle {
-    content = "Quiz completed",
-    contentBold = 'You scored...'
-}
 
 export const StartMenuPage = () => {
-    const statut = false;
     const {quizzes} = useDataStore();
     const backgroundColorIcon = ['#FFF1E9', '#E0FDEF','#EBF0FF','#F6E7FF'];
     return (
         <>
         <Header/>
             <div className={'flex flex-col gap-10 pt-4 m-2'}>
-            {!statut ?
                 <Title content={StartTitle.content} contentBold={StartTitle.contentBold}/>
-                :
-                <Title content={EndTitle.content} contentBold={EndTitle.contentBold}/> }
-
-            <div className={"flex flex-col justify-center"}>
-                {quizzes.map((quiz, index) => (
-                    <ButtonSubject
-                        icon={quiz.icon}
-                        key={index}
-                        titleSubject={quiz.title}
-                        backgroundColor={backgroundColorIcon[index % backgroundColorIcon.length]}
-                    />
-                ))}
-
-            </div>
+                <div className={"flex flex-col justify-center"}>
+                    {quizzes.map((quiz, index) => (
+                        <ButtonSubject
+                            icon={quiz.icon}
+                            key={index}
+                            titleSubject={quiz.title}
+                            backgroundColor={backgroundColorIcon[index % backgroundColorIcon.length]}
+                        />
+                    ))}
+                </div>
             </div>
         </>
     );
