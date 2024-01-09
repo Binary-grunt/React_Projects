dd461d68af72d3e405c22561cfa4c42c69246fd6
C_elare <houvillev@gmail.com>
2024-01-09 04:12:27 +0100

    feat(Quiz): Add QuestionQuiz component
    - Add new file QuestionQuiz.tsx
    - Implement QuestionQuiz component with props:
      - subTextColor: string
      - currentIndexQuestion: number
      - quizQuestionLength: number
      - quizQuestion: string
    
    The QuestionQuiz component displays a question with its index and the total number of questions.
    
    Issue: main

---
+++ src/components/Quiz/QuestionQuiz.tsx
@@ -0,0 +1,25 @@
+import {FC} from "react";
+
+type QuestionQuizProps = {
+    subTextColor: string,
+    currentIndexQuestion: number,
+    quizQuestionLength: number,
+    quizQuestion: string,
+}
+export const QuestionQuiz:FC<QuestionQuizProps> = (
+    {subTextColor, currentIndexQuestion, quizQuestionLength, quizQuestion }) => {
+    return (
+        <>
+            <div>
+                <p style={{color: subTextColor}}
+                   className={'font-rubik italic font-light'}
+                >
+                    Question {currentIndexQuestion + 1} of {quizQuestionLength}
+                </p>
+                <h2 className={'text-2xl font-rubik'}>
+                    {quizQuestion}
+                </h2>
+            </div>
+        </>
+    );
+};
