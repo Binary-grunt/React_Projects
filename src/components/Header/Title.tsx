e273b4f0b30c57e6996b0a8782ff9d3dee31ca46
C_elare <houvillev@gmail.com>
2024-01-09 03:28:56 +0100

    feat(Header): add font styling to Title component
    - Add font styling to the Title component in the Header
    - Update the className of the text element to include the 'font-rubik' class
    - Update the className of the div element to include the 'font-rubik' class
    
    Issue: main

--- src/components/Header/Title.tsx
+++ src/components/Header/Title.tsx
@@ -10,7 +10,7 @@ export const Title:FC<Title>= ({content, contentBold}) => {
     return (
         <>
             <div className={'flex flex-col font-rubik font-light gap-3 m-7'}>
-                <div className={'text-5xl'}> <h1>{content}</h1>
+                <div className={'text-5xl font-rubik'}> <h1>{content}</h1>
                     <div className={'font-rubik font-normal'}>
                     {contentBold}
                     </div>
