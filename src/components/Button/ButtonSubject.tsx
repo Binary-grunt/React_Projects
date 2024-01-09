90021bdae9c5ed1b8b6984eb1079bba6eef37ca0
C_elare <houvillev@gmail.com>
2024-01-09 04:12:00 +0100

    feat(button): rename ButtonSubject component (main)

--- src/components/Button/ButtonSubject.tsx
+++ src/components/Button/ButtonSubject.tsx
@@ -1,5 +1,5 @@
 import { FC } from "react";
-import { useThemeStore } from "../../../store/themeStore.tsx";
+import { useThemeStore } from "../../store/themeStore.tsx";
 
 type ButtonSubjectProps = {
     icon?: string;
@@ -30,15 +30,16 @@ export const ButtonSubject: FC<ButtonSubjectProps> = (
                     <span className={'ml-2 font-rubik font-semibold text-lg pl-2'}>{titleSubject}</span>
                 </button>
             )}
+
             {options && options.map((option, index) => (
                 <button
                     key={index}
                     style={{ backgroundColor: themeButton }}
-                    className="flex flex-row items-center rounded-xl mx-6 my-2 p-3 bg-violet-500 hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300"
+                    className="flex flex-row items-center rounded-xl  my-2 p-3 bg-violet-500 hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300"
                 >
                     <div
                         className={'rounded-lg p-2 text-xl font-rubik w-10 '}
-                        style={{ backgroundColor, color: '#626C7F' }}>
+                        style={{ backgroundColor, color: '#626C7F'}}>
                         {letters[index % letters.length]}
                     </div>
                     <div className="ml-2 font-rubik text-lg pl-2">
