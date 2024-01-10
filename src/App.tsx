7d05327bb760b31d6c82c19f41e68e6ef8140f22
C_elare <houvillev@gmail.com>
2024-01-10 11:05:48 +0100

    feat(App): Add routing functionality to App component (main)

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import {FC} from 'react';
 import './App.css';
 import {StartMenuPage} from "./pages/StartMenuPage.tsx";
-import {BackGroundIcon} from "./themes/BackGroundIcon.tsx";
 import {QuizPage} from "./pages/QuizPage.tsx";
+import {Routes, Route, BrowserRouter} from "react-router-dom";
 
 export const App: FC = () => {
 
     return (
-            <BackGroundIcon>
-                <QuizPage/>
-                {/*<StartMenuPage />*/}
-            </BackGroundIcon>
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<StartMenuPage />} />
+                    <Route path="/:index" element={<QuizPage/>} />
+                </Routes>
+            </BrowserRouter>
+        </>
     );
 }
