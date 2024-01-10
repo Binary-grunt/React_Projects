d318eb4aea1db2720d49683bde26fccc6c91320d
C_elare <houvillev@gmail.com>
2024-01-10 11:05:58 +0100

    feat(ButtonSubmit): Increase button padding (main)

--- src/components/Button/ButtonSubmit.tsx
+++ src/components/Button/ButtonSubmit.tsx
@@ -10,7 +10,7 @@ export const ButtonSubmit:FC<ButtonSubmitProps> = ({ onSubmitClick, content}) =>
             <button
                 type='submit'
                 style={{color: '#FFF'}}
-                className={'bg-violet-600 rounded-xl my-2 p-3 font-rubik text-lg shadow-lg'}
+                className={'bg-violet-600 rounded-xl my-3 p-4 font-rubik text-lg shadow-lg'}
                 onClick={onSubmitClick}
             >
                 {content}
