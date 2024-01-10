0189c4ee9d8bc988ce7b9d4370aa47de88ec650e
C_elare <houvillev@gmail.com>
2024-01-10 11:06:23 +0100

    feat(Quiz): Add play again functionality (main)
    - Add useNavigate hook from react-router-dom
    - Create handlePlayAgainClick function to navigate to home page
    - Update score display size and position

--- src/components/Quiz/ScoreFinal.tsx
+++ src/components/Quiz/ScoreFinal.tsx
@@ -2,6 +2,7 @@ import {Title} from "../Header/Title.tsx";
 import {FC, ReactNode} from "react";
 import {ButtonSubmit} from "../Button/ButtonSubmit.tsx";
 import {useThemeStore} from "../../store/themeStore.tsx";
+import {useNavigate} from "react-router-dom";
 
 type ScoreFinalProps = {
     score: number,
@@ -10,6 +11,10 @@ type ScoreFinalProps = {
 }
 export const ScoreFinal: FC<ScoreFinalProps> = ({ score, children, quizQuestionLength }) => {
     const { themeButton } = useThemeStore();
+    const navigate = useNavigate();
+    const handlePlayAgainClick = () => {
+        navigate('/'); // Navigue vers la page d'accueil
+    };
     return (
         <>
             <Title content={'Quiz completed'} contentBold={'You scored...'}/>
@@ -17,13 +22,13 @@ export const ScoreFinal: FC<ScoreFinalProps> = ({ score, children, quizQuestionL
                 <div className={`md:container md:mx-auto font-rubik shadow-lg rounded-xl flex flex-col items-center justify-center w-11/12 p-10`}
                      style={{backgroundColor: themeButton}}>
                     {children}
-                    <div className="text-center pt-9">
-                        <span className="text-6xl">{score}</span>
-                        <p className="pt-4">out of {quizQuestionLength}</p>
+                    <div className="text-center pt-5">
+                        <span className="text-8xl">{score}</span>
+                        <p className="pt-2">out of {quizQuestionLength}</p>
                     </div>
                 </div>
-                <div>
-                    <ButtonSubmit content={'Play again'}/>
+                <div className={'pt-3'}>
+                    <ButtonSubmit content={'Play again'} onSubmitClick={handlePlayAgainClick}/>
                 </div>
             </div>
         </>
