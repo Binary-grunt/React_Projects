719836785fc95d8fa87b6c183c80c770dac16b5d
C_elare <houvillev@gmail.com>
2024-01-10 05:32:34 +0100

    feat(Quiz): Add final score component
    - Add ScoreFinal component to display the final score of the quiz
    - Import useThemeStore from themeStore.tsx
    - Use useThemeStore to get the themeButton value
    - Update ScoreFinalProps type to include quizQuestionLength
    - Update ScoreFinal component to use the updated props
    - Update the layout and styling of the ScoreFinal component
    - Display the final score and the total number of quiz questions
    - Add a "Play again" button
    
    Issue: main

--- src/components/Quiz/ScoreFinal.tsx
+++ src/components/Quiz/ScoreFinal.tsx
@@ -1,30 +1,30 @@
 import {Title} from "../Header/Title.tsx";
 import {FC, ReactNode} from "react";
 import {ButtonSubmit} from "../Button/ButtonSubmit.tsx";
+import {useThemeStore} from "../../store/themeStore.tsx";
 
 type ScoreFinalProps = {
     score: number,
     children: ReactNode,
     quizQuestionLength: number,
 }
-export const ScoreFinal:FC<ScoreFinalProps> = ({score, children, quizQuestionLength}) => {
+export const ScoreFinal: FC<ScoreFinalProps> = ({ score, children, quizQuestionLength }) => {
+    const { themeButton } = useThemeStore();
     return (
         <>
             <Title content={'Quiz completed'} contentBold={'You scored...'}/>
-
-            <div>
-                <div>
-                {children}
-                </div>
-                <div>
-                    <div>
-                        <span>{score}</span>
-                        <p>out of {quizQuestionLength}</p>
+            <div className="flex flex-col justify-center items-center p-6">
+                <div className={`md:container md:mx-auto font-rubik shadow-lg rounded-xl flex flex-col items-center justify-center w-11/12 p-10`}
+                     style={{backgroundColor: themeButton}}>
+                    {children}
+                    <div className="text-center pt-9">
+                        <span className="text-6xl">{score}</span>
+                        <p className="pt-4">out of {quizQuestionLength}</p>
                     </div>
                 </div>
-                <ButtonSubmit
-                    content={'Play again'}
-                />
+                <div>
+                    <ButtonSubmit content={'Play again'}/>
+                </div>
             </div>
         </>
     );
