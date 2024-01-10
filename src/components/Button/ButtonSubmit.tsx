5634fa27c417ac338e18387f791aa88e647b9ec9
C_elare <houvillev@gmail.com>
2024-01-10 04:41:28 +0100

    feat(ButtonSubmit): add shadow to button (main)

--- src/components/Button/ButtonSubmit.tsx
+++ src/components/Button/ButtonSubmit.tsx
@@ -1,18 +1,19 @@
 import {FC} from "react";
 
 type ButtonSubmitProps = {
-    onSubmitClick: () => void;
+    content: string;
+    onSubmitClick?: (e:never) => void;
 }
-export const ButtonSubmit:FC<ButtonSubmitProps> = ({onSubmitClick}) => {
+export const ButtonSubmit:FC<ButtonSubmitProps> = ({ onSubmitClick, content}) => {
     return (
         <>
             <button
                 type='submit'
                 style={{color: '#FFF'}}
-                className={'bg-violet-600 rounded-xl my-2 p-3 font-rubik text-lg'}
+                className={'bg-violet-600 rounded-xl my-2 p-3 font-rubik text-lg shadow-lg'}
                 onClick={onSubmitClick}
             >
-                Submit Answer
+                {content}
             </button>
         </>
     );
