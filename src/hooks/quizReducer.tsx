42c29f1ceee4de939c34dd4acc4eb3e1a2b11bff
C_elare <houvillev@gmail.com>
2024-01-10 04:42:41 +0100

    feat(quizReducer): Add quizReducer file
    - Add quizReducer file with InitialState and ActionType types
    - Implement quizReducer function with SELECT_OPTION, SUBMIT_ANSWER,
      NEXT_QUESTION, and RESET actions
    - Update state based on the action type and payload
    
    Issue: main

---
+++ src/hooks/quizReducer.tsx
@@ -0,0 +1,45 @@
+type InitialState = {
+    currentQuestionIndex: number,
+    selectedOption: string,
+    isSubmit: boolean,
+    isCorrect: boolean,
+    score: number,
+};
+
+type ActionType =
+    | {type: 'SELECT_OPTION', payload: string}
+    | {type: 'SUBMIT_ANSWER', payload: boolean }
+    | {type: 'NEXT_QUESTION'}
+    | {type: 'RESET', payload: InitialState}
+
+
+export const quizReducer = (state:InitialState, action:ActionType) => {
+    let updatedScore;
+    switch (action.type){
+        case 'SELECT_OPTION':
+            return {...state,
+                selectedOption:
+                action.payload,
+                isSubmit: false
+            };
+        case 'SUBMIT_ANSWER':
+            updatedScore = action.payload ? state.score + 1 : state.score;
+            return {...state,
+                isSubmit: true,
+                isCorrect: action.payload,
+                score: updatedScore
+            };
+        case 'NEXT_QUESTION':
+            return { ...state,
+                currentQuestionIndex: state.currentQuestionIndex + 1,
+                selectedOption: '',
+                isSubmit: false,
+                isCorrect: false,
+                score: state.score
+            };
+        case 'RESET':
+            return action.payload;
+        default:
+            return state;
+    }
+}
