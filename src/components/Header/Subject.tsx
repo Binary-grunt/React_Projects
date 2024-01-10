5887a879e4ead69645063540318b01bf1d5fc111
C_elare <houvillev@gmail.com>
2024-01-10 04:42:30 +0100

    feat(Header): add shadow to subject icon (main)

--- src/components/Header/Subject.tsx
+++ src/components/Header/Subject.tsx
@@ -9,7 +9,7 @@ export const Subject: FC <SubjectTitleType> = ({icon, title, backgroundColor}) =
     return (
         <>
             <div className={'flex flex-row content-center font-rubik font-normal items-center'}>
-                <img className={'rounded-lg p-2 mr-4 size-12'} src={icon} alt={title} style={{backgroundColor }}/>
+                <img className={'rounded-lg p-2 mr-4 size-12 shadow-lg'} src={icon} alt={title} style={{backgroundColor }}/>
                 <h2 className={'text-xl'}>{title}</h2>
             </div>
 
