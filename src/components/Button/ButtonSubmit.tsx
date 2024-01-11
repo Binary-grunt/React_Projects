4c4f705166cb9e92d98eda329a1c25ee258bddb6
C_elare <houvillev@gmail.com>
2024-01-11 05:17:40 +0100

    feat(ButtonSubmit): Add disabled prop to ButtonSubmit component (main)

--- src/components/Button/ButtonSubmit.tsx
+++ src/components/Button/ButtonSubmit.tsx
@@ -2,9 +2,10 @@ import {FC} from "react";
 
 type ButtonSubmitProps = {
     content: string;
+    disabled?: boolean,
     onSubmitClick?: (e:never) => void;
 }
-export const ButtonSubmit:FC<ButtonSubmitProps> = ({ onSubmitClick, content}) => {
+export const ButtonSubmit:FC<ButtonSubmitProps> = ({ onSubmitClick, content, disabled}) => {
     return (
         <>
             <button
@@ -13,6 +14,7 @@ export const ButtonSubmit:FC<ButtonSubmitProps> = ({ onSubmitClick, content}) =>
                 className={'bg-violet-600 rounded-2xl my-3 p-5 font-rubik text-lg shadow-lg ' +
                     'md:text-3xl md:my-5 md:p-8'}
                 onClick={onSubmitClick}
+                disabled={disabled}
             >
                 {content}
             </button>
