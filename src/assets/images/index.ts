83719e185afecd44813e068cdf90c32a3176e62c
C_elare <houvillev@gmail.com>
2024-01-11 04:21:56 +0100

    feat(images): add image exports (main)

---
+++ src/assets/images/index.ts
@@ -0,0 +1,11 @@
+// Dans assets/images/index.ts
+export { default as MobileIconLight } from './pattern-background-mobile-light.svg';
+export { default as MobileIconDark } from './pattern-background-mobile-dark.svg';
+export { default as TabletIconLight } from './pattern-background-tablet-light.svg';
+export { default as TabletIconDark } from './pattern-background-tablet-dark.svg';
+export { default as DesktopIconLight } from './pattern-background-desktop-light.svg';
+export { default as DesktopIconDark } from './pattern-background-desktop-dark.svg';
+export { default as MoonLight } from './icon-moon-light.svg';
+export { default as MoonDark } from './icon-moon-dark.svg';
+export { default as SunLight } from './icon-sun-light.svg';
+export { default as SunDark } from './icon-sun-dark.svg';
