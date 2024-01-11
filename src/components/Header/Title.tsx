6b62d29226e5902374651594d7f729f2ea9c68fb
C_elare <houvillev@gmail.com>
2024-01-11 04:13:59 +0100

    feat(Header): Update Title component styling
    - Updated the padding-top of the Title component to 6
    - Increased the font size of the main title to 7xl on medium screens
    
    Issue: main

--- src/components/Header/Title.tsx
+++ src/components/Header/Title.tsx
@@ -10,10 +10,10 @@ export const Title:FC<Title>= ({content, contentBold, subTitle}) => {
     const {subTextColor} = useThemeStore();
     return (
         <>
-            <div className={'flex flex-col font-rubik font-light gap-3 m-7'}>
-                <div className={'text-5xl font-rubik'}>
+            <div className={'flex flex-col font-rubik font-light pt-6'}>
+                <div className={'text-5xl font-rubik md:text-7xl'}>
                     <h1>{content}</h1>
-                    <div className={'font-rubik font-normal'}>
+                    <div className={'font-rubik font-normal'} >
                     {contentBold}
                     </div>
                 </div>
