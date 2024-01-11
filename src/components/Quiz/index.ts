f2b80a1b04f44f22950c95c8381e1b886f6d3563
C_elare <houvillev@gmail.com>
2024-01-11 04:30:05 +0100

    feat(Quiz): Add QuizChoice component
    - Added QuizChoice component to the Quiz index file.
    - Exported QuizChoice from './QuizChoice.tsx' module.
    - No newline at the end of the file.
    
    Issue: main

--- src/components/Quiz/index.ts
+++ src/components/Quiz/index.ts
@@ -1,4 +1,5 @@
 export { InputQuiz } from "./InputQuiz";
 export { ProgressBar } from "./ProgressBar";
 export { QuestionQuiz } from "./QuestionQuiz";
+export {QuizChoice} from './QuizChoice.tsx'
 export {Score} from './Score.tsx';
