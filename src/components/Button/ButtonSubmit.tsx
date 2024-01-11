6bd3f707b84320642743bae436a89be02c3febf9
C_elare <houvillev@gmail.com>
2024-01-11 02:17:10 +0100

    feat(ButtonSubmit): Update button styling for different screen sizes
    - Update className in ButtonSubmit component to include additional
      styles for medium and large screen sizes.
    - Change rounded-xl to rounded-2xl for a more rounded button shape.
    - Increase padding and font size for better visibility on larger screens.
    
    Issue: #123

--- src/components/Button/ButtonSubmit.tsx
+++ src/components/Button/ButtonSubmit.tsx
@@ -10,7 +10,8 @@ export const ButtonSubmit:FC<ButtonSubmitProps> = ({ onSubmitClick, content}) =>
             <button
                 type='submit'
                 style={{color: '#FFF'}}
-                className={'bg-violet-600 rounded-xl my-3 p-4 font-rubik text-lg shadow-lg'}
+                className={'bg-violet-600 rounded-2xl my-3 p-5 font-rubik text-lg shadow-lg ' +
+                    'md:text-3xl md:my-5 md:p-8'}
                 onClick={onSubmitClick}
             >
                 {content}
