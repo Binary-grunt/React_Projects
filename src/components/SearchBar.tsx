c56130a8e3bb7a931e07f1041a8c698b846d8219
C_elare <houvillev@gmail.com>
2024-01-12 12:27:53 +0100

    feat(SearchBar): Add error message when no results found

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -8,30 +8,35 @@ type SearchBarProps = {
 
 export const SearchBar:FC<SearchBarProps> = ({onSearch}) => {
     const [searchQuery, setSearchQuery] = useState('');
-    const {secondaryBg} = useThemeStore();
+    const [result, setResult] = useState(true);
+    const {secondaryText, secondaryBg} = useThemeStore();
 
     const handleChange = (e:ChangeEvent<HTMLInputElement> ) => {
         setSearchQuery(e.target.value);
+        setResult(true);
     }
 
     const handleSubmit = (e:FormEvent<HTMLFormElement> ) => {
         e.preventDefault();
-        onSearch(searchQuery);
+        searchQuery ? onSearch(searchQuery) : setResult(false);
     };
 
     return (
         <>
-            <form onSubmit={handleSubmit} className={`${secondaryBg} flex flex-row justify-between p-4 rounded-xl shadow-md`}>
-                <img src={IconSearch} alt='Icon search'/>
+            <form onSubmit={handleSubmit}
+                  className={`flex flex-row justify-between items-center font-spacemono`}>
+                <img src={IconSearch} alt='Icon search' className={'size-fit pl-2'}/>
                 <input
                     type="text"
                     value={searchQuery}
                     placeholder='Search GitHub username...'
                     onChange={handleChange}
+                    className={`w-full bg-none pl-3 focus:outline-none ${secondaryBg} ${secondaryText}`}
                 />
+                {!result && <span className={'text-dev-red text-sm mr-2 font-bold'}>No results</span>}
                 <button
                     type='submit'
-                    className={'bg-dev-blue text-lightMode-white'}
+                    className={'bg-dev-blue text-lightMode-white rounded-xl h-12 w-32'}
                 > Search </button>
             </form>
 
