54b5a0cf3f34fe3158c2538f87172e9761a43853
C_elare <houvillev@gmail.com>
2024-01-12 13:52:05 +0100

    feat(PseudoGitHub): Add PseudoGitHub component
    Add a new file PseudoGitHub.tsx to the src/components/ContainerGitHub
    directory. The component displays GitHub user data and includes a
    formatDate function to format the creation date. The component uses
    the useThemeStore hook to access theme-related data. The component
    renders the user's avatar, name, login, and join date.
    
    Issue: main

---
+++ src/components/ContainerGitHub/PseudoGitHub.tsx
@@ -0,0 +1,30 @@
+import {GitHubUserData} from "../../pages/GitHubUser.tsx";
+import {useThemeStore} from "../../store/themeStore.tsx";
+import {FC} from "react";
+
+type PseudoGitHubProps = {
+    userData: GitHubUserData
+}
+
+const formatDate = (dateString: string): string => {
+    const dateObject = new Date(dateString);
+    const options: Intl.DateTimeFormatOptions = { year: 'numeric', month: 'short', day: 'numeric' };
+    return dateObject.toLocaleDateString('en-US', options);
+}
+
+export const PseudoGitHub:FC<PseudoGitHubProps> = ({userData}) => {
+    const {primaryText, secondaryText} = useThemeStore();
+    const formattedDate: string = formatDate(userData.created_at);
+    return (
+        <>
+            <div className={'flex flex-row pb-9'}>
+                <img src={userData.avatar_url} alt='Avatar' className={'rounded-full size-24 md:size-36'}/>
+                <div className={'flex flex-col items-start pl-5 md:pl-14 lg:pl-12'}>
+                    <span className={`${secondaryText} font-bold text-2xl md:text-4xl md:pt-4`}>{userData.name}</span>
+                    <span className={'text-dev-blue pb-2 md:text-lg md:pt-2'}>@{userData.login}</span>
+                    <p className={`${primaryText} text-sm md:text-lg`}>Joined {formattedDate}</p>
+                </div>
+            </div>
+        </>
+    );
+};
