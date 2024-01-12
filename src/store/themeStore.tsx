67e90b47c6d53a4d1fa4788c593450efa96bb552
C_elare <houvillev@gmail.com>
2024-01-13 00:06:10 +0100

    feat(theme): Add toggleTheme function and initial theme states (main)

--- src/store/themeStore.tsx
+++ src/store/themeStore.tsx
@@ -27,20 +27,29 @@ type ThemeStore = {
     toggleTheme: () => void;
 }
 
+/**
+ * Theme store for managing light and dark themes.
+ * Provides states and functions to toggle and manage the application's theme.
+ */
+
 export const useThemeStore = create<ThemeStore>((set) => ({
+    // Initial theme states
     theme: 'light',
     iconTheme: IconMoon,
     themeText: 'DARK',
+    // Initial values for light theme
     primaryBg: ThemeLight.bg_light_gray,
     primaryText: ThemeLight.text_gray,
     secondaryBg: ThemeLight.bg_light_white,
     secondaryText: ThemeLight.text_black,
     inactiveText: ThemeLight.inactive_gray,
 
+    // Function to toggle between light and dark themes
     toggleTheme: () => set(state => ({
         theme: state.theme === 'light' ? 'dark' : 'light',
         iconTheme: state.theme === 'light' ? IconSun : IconMoon,
         themeText: state.theme === 'light' ? 'LIGHT' : 'DARK',
+        // Switching theme values based on current theme
         primaryBg: state.theme === 'light' ? ThemeDark.bg_dark : ThemeLight.bg_light_gray,
         secondaryBg: state.theme === 'light' ? ThemeDark.bg_darkblue : ThemeLight.bg_light_white,
         primaryText: state.theme === 'light' ? ThemeDark.text_white : ThemeLight.text_gray,
