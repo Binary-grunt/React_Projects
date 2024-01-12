3bdc67f7d1a7b90fc79515644ec9e5793b7a69bd
C_elare <houvillev@gmail.com>
2024-01-12 23:42:34 +0100

    feat(PseudoGitHub): Refactor PseudoGitHub component
    Refactor the PseudoGitHub component to improve readability and maintainability.
    - Reformat the code to follow consistent spacing and indentation.
    - Update class names to use tailwind CSS utility classes.
    - Adjust image size and alignment.
    - Add comments for better code documentation.
    
    Issue: main

--- src/components/ContainerGitHub/PseudoGitHub.tsx
+++ src/components/ContainerGitHub/PseudoGitHub.tsx
@@ -12,35 +12,38 @@ const formatDate = (dateString: string): string => {
     return dateObject.toLocaleDateString('en-US', options);
 }
 
-export const PseudoGitHub:FC<PseudoGitHubProps> = ({userData}) => {
-    const {primaryText, secondaryText, inactiveText} = useThemeStore();
-    const formattedDate: string = formatDate(userData.created_at);
+export const PseudoGitHub: FC<PseudoGitHubProps> = ({ userData }) => {
+    const { primaryText, secondaryText, inactiveText } = useThemeStore();
+    const formattedDate = formatDate(userData.created_at);
 
     return (
-        <>
-            <div className={' pb-9 lg:pb-0'}>
-                <div className={'grid grid-cols-3 justify-center items-center lg:grid-flow-row-dense'}>
-                    <img src={userData.avatar_url} alt='Avatar' className={'rounded-full size-24 md:size-36 lg:row-span-3 '}/>
-                    <div className={` flex flex-col items-start justify-center 
-                    lg:grid lg:grid-cols-2 lg:justify-between `}>
-                        <span className={`${secondaryText} font-bold text-2xl md:text-4xl  `}>
-                            {userData.name}
-                        </span>
-                        <span className={'text-dev-blue pb-1 md:text-lg md:pt-2 lg:order-3 lg:pb-6'}>
-                            @{userData.login}
-                        </span>
-                        <p className={`${primaryText} text-sm md:text-lg lg:ml-2 lg:order-2`}>
-                            Joined {formattedDate}
-                        </p>
-                    </div>
-                    <div className={`col-span-3 lg:order-last lg:col-span-2`}>
-                        <p className={`${inactiveText} leading-7 text-sm md:text-lg`}>
-                            {userData.bio ? userData.bio : 'This profile has no bio'}
-                        </p>
-                    </div>
-                </div>
+        <div className="pb-9 flex flex-col">
+            <div className="flex flex-row items-center gap-4 lg:flex-row">
+                <img
+                    src={userData.avatar_url}
+                    alt="Avatar"
+                    className="rounded-full w-24 h-24 md:w-36 md:h-36 lg:w-40 lg:h-40"
+                />
 
+                <div className="flex flex-col items-start pl-4 lg:flex-grow">
+                    <span className={`${secondaryText} font-bold text-2xl md:text-4xl`}>
+                        {userData.name}
+                    </span>
+                    <span className="text-dev-blue pb-1 md:text-lg md:pt-2">
+                        @{userData.login}
+                    </span>
+                    <p className={`${primaryText} text-sm md:text-lg `}>
+                        Joined {formattedDate}
+                    </p>
+                </div>
             </div>
-        </>
+
+            <p className={`${inactiveText} leading-7 text-sm mt-4 md:text-lg lg:pl-48`}>
+                {userData.bio ? userData.bio : 'This profile has no bio'}
+            </p>
+        </div>
     );
 };
+
+
+
