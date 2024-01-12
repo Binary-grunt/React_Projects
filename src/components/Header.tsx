e61bd1e7a4f86f8497396f1ecc38cfb6d3654274
C_elare <houvillev@gmail.com>
2024-01-12 13:59:59 +0100

    feat(Header): Update Header component styling (main)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,18 @@
-
 import {useThemeStore} from "../store/themeStore.tsx";
-
 
 export const Header = () => {
     const {toggleTheme, iconTheme, themeText, primaryText, secondaryText} = useThemeStore();
+
     return (
         <>
             <div className={'flex flex-row justify-between'}>
-                <h1 className={`${secondaryText} font-spacemono font-bold text-3xl`}>devfinder</h1>
+                <h1 className={`${secondaryText} font-spacemono font-bold text-3xl`}>
+                    devfinder
+                </h1>
                     <button onClick={toggleTheme} className={'flex flex-row items-center'}>
-                        <p className={`${primaryText} font-spacemono text-lg tracking-widest pr-4`}>{themeText}</p>
+                        <p className={`${primaryText} font-spacemono text-lg tracking-widest pr-4`}>
+                            {themeText}
+                        </p>
                         <img src={iconTheme} alt='Icon-theme'/>
                     </button>
             </div>
