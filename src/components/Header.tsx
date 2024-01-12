b70b840ccd37269021bc717206b89e4088efe7e3
C_elare <houvillev@gmail.com>
2024-01-12 23:41:31 +0100

    feat(Header): update Header component styling
    - Updated the styling of the Header component in Header.tsx.
    - Adjusted the font size and spacing for the title.
    - Added responsive styling for different screen sizes.
    - Updated the button styling and added spacing between elements.
    
    Issue: main

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,26 @@
-import {useThemeStore} from "../store/themeStore.tsx";
+import { useThemeStore } from "../store/themeStore.tsx";
 
 export const Header = () => {
     const {toggleTheme, iconTheme, themeText, primaryText, secondaryText} = useThemeStore();
 
     return (
-        <>
-            <div className={'flex flex-row justify-between'}>
-                <h1 className={`${secondaryText} font-spacemono font-bold text-3xl`}>
-                    devfinder
-                </h1>
-                    <button onClick={toggleTheme} className={'flex flex-row items-center'}>
-                        <p className={`${primaryText} font-spacemono text-lg tracking-widest pr-4`}>
-                            {themeText}
-                        </p>
-                        <img src={iconTheme} alt='Icon-theme'/>
-                    </button>
-            </div>
-        </>
+        <div className="flex justify-between items-center px-4 py-2 md:px-6 md:py-4">
+            <h1 className={`${secondaryText} font-spacemono font-bold text-2xl md:text-3xl`}>
+                devfinder
+            </h1>
+            <button
+                onClick={toggleTheme}
+                className="flex items-center space-x-2"
+            >
+                <p className={`${primaryText} font-spacemono text-base md:text-lg tracking-widest`}>
+                    {themeText}
+                </p>
+                <img
+                    src={iconTheme}
+                    alt="Icon theme"
+                    className="w-6 h-6 md:w-8 md:h-8"
+                />
+            </button>
+        </div>
     );
 };
