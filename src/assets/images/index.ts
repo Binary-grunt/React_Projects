5539c656daaf88213ccc0cff193076ba7b727b22
C_elare <houvillev@gmail.com>
2024-01-13 01:02:57 +0100

    feat(images): Add TodoIcon and fix newline issue (main)

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -6,3 +6,4 @@ export {default as IconCheck} from './icon-check.svg';
 export {default as IconCross} from './icon-cross.svg';
 export {default as IconMoon} from './icon-moon.svg';
 export {default as IconSun} from './icon-sun.svg';
+export {default as TodoIcon} from './TODO_icon.svg';
