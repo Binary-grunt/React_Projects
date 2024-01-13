0841c494ecc2a93dbb21c3b85e65ab6e4daeeea5
C_elare <houvillev@gmail.com>
2024-01-13 04:34:45 +0100

    feat(theme): add theme constants (main)

---
+++ src/constants/themeConstants.ts
@@ -0,0 +1,19 @@
+// Themes constants
+export const themes = {
+    light: {
+        primaryBg: 'bg-light-theme-very-light-gray',
+        secondaryBg: 'bg-light-theme-very-light-white',
+        contentColor: 'bg-light-theme-light-grayish-blue',
+        primaryText: 'text-light-theme-very-dark-grayish-blue',
+        secondaryText: 'text-light-theme-dark-grayish-blue',
+        inactiveText: 'text-light-theme-medium-grayish-blue',
+    },
+    dark: {
+        primaryBg: 'bg-dark-theme-very-dark-blue',
+        secondaryBg: 'bg-dark-theme-dark-desaturated-blue',
+        contentColor: 'bg-dark-theme-medium-grayish-blue-hover',
+        primaryText: 'text-dark-theme-light-grayish-blue',
+        secondaryText: 'text-dark-theme-dark-grayish-blue',
+        inactiveText: 'text-dark-theme-very-dark-grayish-blue',
+    }
+};
