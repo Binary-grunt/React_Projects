ead9edcdfb38930c04ce39104e5d5ea8b4d3982c
C_elare <houvillev@gmail.com>
2024-01-13 07:27:46 +0100

    feat(SearchBar): Update className in form and img tags
    - Update className in form tag to "flex items-center space-x-2 p-1 md:space-x-4 font-spacemono"
    - Update className in img tag to "w-6 h-6 md:w-8 md:h-8"
    - Update className in input tag to "flex-grow bg-transparent text-sm md:text-lg pl-2 focus:outline-none {secondaryBg} {secondaryText}"
    - Update className in span tag to "text-dev-red text-sm font-bold"
    - Update className in button tag to "bg-dev-blue hover:bg-dev-light-blue text-lightMode-white text-sm font-semibold rounded-xl h-12 w-24 md:h-14 md:w-36 transition duration-200"
    
    Issue: main

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -46,12 +46,12 @@ export const SearchBar: FC<SearchBarProps> = ({ onSearch }) => {
     return (
         <form
             onSubmit={handleSubmit}
-            className="flex items-center space-x-2 p-2 md:space-x-4 font-spacemono"
+            className={"flex items-center space-x-2 p-1 md:space-x-4 font-spacemono"}
         >
             <img
                 src={IconSearch}
                 alt="Icon search"
-                className="w-6 h-6 md:w-8 md:h-8"
+                className={"w-6 h-6 md:w-8 md:h-8"}
             />
             <input
                 type="text"
@@ -61,13 +61,13 @@ export const SearchBar: FC<SearchBarProps> = ({ onSearch }) => {
                 className={`flex-grow bg-transparent text-sm md:text-lg pl-2 focus:outline-none ${secondaryBg} ${secondaryText}`}
             />
             {!result && (
-                <span className="text-dev-red text-sm font-bold">
+                <span className={"text-dev-red text-sm font-bold"}>
                     No results
                 </span>
             )}
             <button
                 type="submit"
-                className="bg-dev-blue hover:bg-dev-light-blue text-lightMode-white text-lg font-semibold rounded-xl h-14 w-24 md:h-14 md:w-36 transition duration-200"
+                className={"bg-dev-blue hover:bg-dev-light-blue text-lightMode-white text-sm font-semibold rounded-xl h-12 w-24 md:h-14 md:w-36 transition duration-200"}
             >
                 Search
             </button>
