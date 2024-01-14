f5aaa6d41454b51204c5b0de01ecc74d24202d48
Celare <119490047+C-elare@users.noreply.github.com>
2024-01-14 05:28:47 +0100

    feat(App): Add functionality to add todos (main)

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,36 @@
 import './App.css'
 import {useThemeStore} from "./store/themeStore.tsx";
-import {ButtonCreateTask} from "./components/ButtonCreateTask.tsx";
+import {AddTodoForm} from "./components/AddTodoForm.tsx";
 import {Header} from "./components/Header.tsx";
+import {useState} from "react";
+import {ListTodo} from "./components/ListTodo.tsx";
+
+
+export type ITodo = {
+    id: number;
+    text: string;
+}
 
 export const App = () => {
   const {themeProps, imageBgMobile} = useThemeStore();
+  const [todos, setTodos] = useState<ITodo[]>([]);
+  const [nextId, setNextId] = useState(1);
 
-    const backgroundStyle = {
+  const addTodoHandler = (text: string) => {
+        setTodos(prevTodos => [...prevTodos, { id: nextId, text }]);
+        setNextId(nextId + 1);
+  };
+
+  const backgroundStyle = {
         backgroundImage: `url(${imageBgMobile})`,
-    };
+  };
 
   return (
     <>
       <div style={backgroundStyle} className={`${themeProps.primaryBg} bg-cover bg-center bg-no-repeat h-52 px-7 font-josefin`}>
           <Header/>
-          <ButtonCreateTask/>
+          <AddTodoForm onAddTodo={addTodoHandler} />
+          <ListTodo todos={todos} />
       <h1>Vite + React</h1>
       <div>
 
