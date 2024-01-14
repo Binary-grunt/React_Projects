d68c9bdb2e9faaceeb03a5f066d75bb4c39c9b54
Celare <119490047+C-elare@users.noreply.github.com>
2024-01-15 00:17:09 +0100

    feat(ListTodo): Refactor ListTodo component and add Tasks and FooterTodo components (main)

--- src/components/Todo/ListTodo.tsx
+++ src/components/Todo/ListTodo.tsx
@@ -1,8 +1,9 @@
 import {FC} from "react";
-import {IconCheck, IconCross} from "../../assets/images";
 import {useThemeStore} from "../../store/themeStore.tsx";
-import {Pagination} from "../Pagination.tsx";
+import {FooterTodo} from "../ui/FooterTodo.tsx";
 import {ITodo} from "../../pages/Todopages.tsx";
+import {Pagination} from "../ui/Pagination.tsx";
+import {Tasks} from "./ListTask.tsx";
 
 type TodoListProps = {
     todos: ITodo[];
@@ -15,49 +16,29 @@ type TodoListProps = {
 
 };
 
-export const ListTodo: FC<TodoListProps> = ({  todos, onDeleteTodo, onCheckTodo, onClearCheck, onShowAll, onShowActive, onShowCompleted, }) => {
-    const {themeProps, checkIcon} = useThemeStore();
+export const ListTodo: FC<TodoListProps> =
+    ({ todos, onDeleteTodo, onCheckTodo, onClearCheck, onShowAll, onShowActive, onShowCompleted }) => {
+    const {themeProps} = useThemeStore();
     const notChecked = todos.filter(todo => !todo.checked).length;
-
 
     return (
         <>
-            <div className={`${themeProps.secondaryBg} rounded-xl mt-5`}>
-                <ul>
-                    {todos.map(todo => (
-                        <div key={todo.id}>
-                            <div  className={`${todo.filter} h-14 flex flex-row items-center justify-between pl-5`}>
-                                <div className={'flex flex-row items-center'}>
-                                    <button
-                                        onClick={() => onCheckTodo(todo.id)}
-                                        className={`${todo.checked ? 'bg-gradient-to-br from-check-bg-start to-check-bg-end' : ''} 
-                                        h-6 w-6 rounded-2xl flex justify-center items-center`}>
-                                        {todo.checked ?
-                                            <img src={IconCheck} alt="Checked" />
-                                            :
-                                            <img src={checkIcon} alt="Unchecked" className='w-6 h-6' />
-                                        }
-                                    </button>
-                                    <li className={`${todo.checked ? 'line-through text-light-theme-dark-grayish-blue opacity-50' : ''} 
-                                    ${themeProps.primaryText} ml-3`}>
-                                        {todo.text}
-                                    </li>
-                                </div>
-                                <button onClick={() => onDeleteTodo(todo.id)}>
-                                    <img src={IconCross} alt='Close todotask' className={'pr-7'} />
-                                </button>
-                            </div>
-                            <div className={`h-0.5 ${todo.filter} ${themeProps.contentColor}`}/>
-                        </div>
-                    ))}
-                </ul>
-                <Pagination notChecked={notChecked} onClearCheck={onClearCheck}></Pagination>
+            <div className={`${themeProps.secondaryBg} rounded-md`}>
+                {todos.length ?
+                    <>
+                        <Tasks
+                            todos={todos}
+                            onDeleteTodo={onDeleteTodo}
+                            onCheckTodo={onCheckTodo}
+                        />
+                        <FooterTodo notChecked={notChecked} onClearCheck={onClearCheck}></FooterTodo>
+                    </> : null }
             </div>
-            <div className={`${themeProps.secondaryText} ${themeProps.secondaryBg} h-16 flex flex-row items-center rounded-xl justify-around mt-5 `}>
-                <button onClick={onShowAll}>All</button>
-                <button onClick={onShowActive}>Active</button>
-                <button onClick={onShowCompleted}>Completed</button>
-            </div>
+            <Pagination
+                onShowAll={onShowAll}
+                onShowActive={onShowActive}
+                onShowCompleted={onShowCompleted}
+            />
         </>
     );
 };
