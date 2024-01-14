16ecbbbd06646a1c28c32bb4d81b1354d9c5dad3
Celare <119490047+C-elare@users.noreply.github.com>
2024-01-15 00:37:43 +0100

    feat(todo): update Tasks component to use individual todo instead of todos array (main)

--- src/components/Todo/Tasks.tsx
+++ src/components/Todo/Tasks.tsx
@@ -4,44 +4,41 @@ import {FC} from "react";
 import {useThemeStore} from "../../store/themeStore.tsx";
 
 type TasksProps = {
-    todos: ITodo[]
+    todo: ITodo;
     onDeleteTodo: (id: number) => void;
     onCheckTodo: (id: number) => void;
 }
 
-export const Tasks:FC<TasksProps> = ({todos, onCheckTodo, onDeleteTodo }) => {
+export const Tasks:FC<TasksProps> = ({todo, onCheckTodo, onDeleteTodo }) => {
     const {themeProps, checkIcon} = useThemeStore();
 
     return (
         <>
-            <ul>{todos.map(todo => (
-                <div key={todo.id}>
-                    <div  className={`${todo.filter} h-14 flex flex-row items-center justify-between pl-5`}>
-                        <div className={'flex flex-row items-center'}>
-                            <button
-                                onClick={() => onCheckTodo(todo.id)}
-                                className={`${todo.checked ?
-                                    'bg-gradient-to-br from-check-bg-start to-check-bg-end' : ''}
-                                    h-6 w-6 rounded-2xl flex justify-center items-center`}>
-                                {todo.checked ?<img src={IconCheck} alt="Checked" />
-                                    :
-                                    <img src={checkIcon} alt="Unchecked" className='w-6 h-6' />
-                                }
-                            </button>
-                            <li className={`${todo.checked ?
-                                'line-through text-light-theme-dark-grayish-blue opacity-50' : ''}
-                                ${themeProps.primaryText} ml-3`}>
-                                {todo.text}
-                            </li>
-                        </div>
-                        <button onClick={() => onDeleteTodo(todo.id)}>
-                            <img src={IconCross} alt='Close todotask' className={'pr-7'}/>
+            <div key={todo.id}>
+                <div  className={`${todo.filter} h-14 flex flex-row items-center justify-between pl-5`}>
+                    <div className={'flex flex-row items-center'}>
+                        <button
+                            onClick={() => onCheckTodo(todo.id)}
+                            className={`${todo.checked ?
+                                'bg-gradient-to-br from-check-bg-start to-check-bg-end' : ''}
+                                h-6 w-6 rounded-2xl flex justify-center items-center`}>
+                            {todo.checked ?<img src={IconCheck} alt="Checked" />
+                                :
+                                <img src={checkIcon} alt="Unchecked" className='w-6 h-6' />
+                            }
                         </button>
+                        <li className={`${todo.checked ?
+                            'line-through text-light-theme-dark-grayish-blue opacity-50' : ''}
+                            ${themeProps.primaryText} ml-3`}>
+                            {todo.text}
+                        </li>
                     </div>
-                    <div className={`h-0.5 ${todo.filter} ${themeProps.contentColor}`}/>
+                    <button onClick={() => onDeleteTodo(todo.id)}>
+                        <img src={IconCross} alt='Close todotask' className={'pr-7'}/>
+                    </button>
                 </div>
-            ))}
-            </ul>
+                <div className={`h-0.5 ${todo.filter} ${themeProps.contentColor}`}/>
+            </div>
         </>
     );
 };
