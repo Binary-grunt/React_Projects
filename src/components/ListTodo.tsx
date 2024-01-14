8041812a72c05e0e341ce5d4ac02ec8c2bb706a2
Celare <119490047+C-elare@users.noreply.github.com>
2024-01-14 09:31:13 +0100

    feat(ListTodo): Add pagination component
    - Add Pagination component to ListTodo component
    - Update props interface to include onClearCheck function
    - Calculate number of unchecked todos
    - Add separator line between todo items
    - Add Pagination component to render unchecked todos count
    - Update styles for better alignment and spacing
    
    Closes main

--- src/components/ListTodo.tsx
+++ src/components/ListTodo.tsx
@@ -2,40 +2,44 @@ import {FC} from "react";
 import {ITodo} from "../App.tsx";
 import {IconCheck, IconCross} from "../assets/images";
 import {useThemeStore} from "../store/themeStore.tsx";
+import {Pagination} from "./Pagination.tsx";
 
 type TodoListProps = {
     todos: ITodo[];
     onDeleteTodo: (id: number) => void;
     onCheckTodo: (id: number) => void;
+    onClearCheck: () => void;
 };
 
-export const ListTodo: FC<TodoListProps> = ({  todos, onDeleteTodo, onCheckTodo  }) => {
+export const ListTodo: FC<TodoListProps> = ({  todos, onDeleteTodo, onCheckTodo, onClearCheck  }) => {
     const {themeProps, checkIcon} = useThemeStore();
+    const notChecked = todos.filter(todo => !todo.checked).length;
 
     return (
-        <div className={`${themeProps.secondaryBg} rounded-xl mt-40`}>
+        <div className={`${themeProps.secondaryBg} rounded-xl mt-5`}>
             <ul>
                 {todos.map(todo => (
-                    <>
-                    <div key={todo.id} className="h-12 flex flex-row items-center justify-between pl-5">
-                        <div className={'flex flex-row items-center'}>
-                            <button
-                                onClick={() => onCheckTodo(todo.id)}
-                                className={`${todo.checked ? 'bg-gradient-to-br from-check-bg-start to-check-bg-end' : ''} h-6 w-6 rounded-2xl flex justify-center items-center`}>
-                                {todo.checked ? <img src={IconCheck} alt="Checked" /> : <img src={checkIcon} alt="Unchecked" className='w-6 h-6' />}
+                    <div key={todo.id}>
+                        <div  className="h-14 flex flex-row items-center justify-between pl-5">
+                            <div className={'flex flex-row items-center'}>
+                                <button
+                                    onClick={() => onCheckTodo(todo.id)}
+                                    className={`${todo.checked ? 'bg-gradient-to-br from-check-bg-start to-check-bg-end' : ''} h-6 w-6 rounded-2xl flex justify-center items-center`}>
+                                    {todo.checked ? <img src={IconCheck} alt="Checked" /> : <img src={checkIcon} alt="Unchecked" className='w-6 h-6' />}
+                                </button>
+                                <li className={`${todo.checked ? 'line-through text-light-theme-dark-grayish-blue opacity-50' : ''} ${themeProps.primaryText} ml-3`}>
+                                    {todo.text}
+                                </li>
+                            </div>
+                            <button onClick={() => onDeleteTodo(todo.id)}>
+                                <img src={IconCross} alt='Close todotask' className={'pr-7'} />
                             </button>
-                            <li className={`${todo.checked ? 'line-through text-light-theme-dark-grayish-blue opacity-50' : ''} ml-3`}>
-                                {todo.text}
-                            </li>
                         </div>
-                        <button onClick={() => onDeleteTodo(todo.id)}>
-                            <img src={IconCross} alt='Close todotask' className={'pr-7'} />
-                        </button>
+                        <div className={`h-0.5 ${themeProps.contentColor}`}/>
                     </div>
-                    <hr/>
-                    </>
                 ))}
             </ul>
+            <Pagination notChecked={notChecked} onClearCheck={onClearCheck}></Pagination>
         </div>
     );
 };
