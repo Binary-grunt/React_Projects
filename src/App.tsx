df91dc972fa879a7d6d30ac5f68a5191d3e31d86
Celare <119490047+C-elare@users.noreply.github.com>
2024-01-14 10:55:14 +0100

    feat(App): Add pagination to ITodo interface
    - Added a new property 'pagination' to the ITodo interface
    - Updated the addTodoHandler function to include the pagination property
    - Created a new state variable 'nextId' to keep track of the next id for new todos
    - Implemented the clearCheckedHandle function to remove checked todos
    - Updated the JSX to include the 'onClearCheck' prop in the ListTodo component
    
    Issue: main

--- src/App.tsx
+++ src/App.tsx
@@ -5,27 +5,35 @@ import {Header} from "./components/Header.tsx";
 import {useState} from "react";
 import {ListTodo} from "./components/ListTodo.tsx";
 
+
 export type ITodo = {
     id: number;
     text: string;
     checked: boolean;
+    pagination: boolean;
 }
 
-const uniqueNumber = Date.now() + Math.random();
 
 export const App = () => {
   const {themeProps, imageBgMobile} = useThemeStore();
   const [todos, setTodos] = useState<ITodo[]>([]);
+  const [nextId, setNextId] = useState(1);
+
 
   const backgroundStyle = {
         backgroundImage: `url(${imageBgMobile})`,
   };
 
-
   const addTodoHandler = (text: string) => {
-        setTodos(prevTodos => [...prevTodos, { id: uniqueNumber, text, checked: false }]);
+        setTodos(prevTodos => [...prevTodos, { id: nextId, text, checked: false, pagination: false }]);
+        setNextId(nextId + 1);
   };
+
   const deleteTodoHandler = (id: number) => setTodos(todos.filter(todo => todo.id !== id));
+
+  const clearCheckedHandle = () => {
+       setTodos(todos.filter( todo => !todo.checked))
+  };
 
   const checkTodoHandler = (id: number) => {
         setTodos(todos.map(todo =>
@@ -33,14 +41,17 @@ export const App = () => {
         ));
   };
 
-  return (
+
+    return (
     <>
+        <div className={`${themeProps.primaryBg} h-screen`}>
       <div style={backgroundStyle} className={`${themeProps.primaryBg} bg-cover bg-center bg-no-repeat h-52 px-7 font-josefin`}>
           <Header/>
           <AddTodoForm onAddTodo={addTodoHandler} />
-          <ListTodo todos={todos} onDeleteTodo={deleteTodoHandler} onCheckTodo={checkTodoHandler} />
+          <ListTodo todos={todos} onDeleteTodo={deleteTodoHandler} onCheckTodo={checkTodoHandler} onClearCheck={clearCheckedHandle} />
 
       </div>
+        </div>
     </>
   )
 }
