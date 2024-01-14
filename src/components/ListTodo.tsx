ece72643151d1c564e7aef6768c78db2d7cb112a
Celare <119490047+C-elare@users.noreply.github.com>
2024-01-14 06:57:04 +0100

    feat(ListTodo): Add delete functionality and improve styling
    - Added onDeleteTodo and onCheckTodo props to ListTodo component
    - Updated ListTodo component to include delete button for each todo item
    - Updated styling for checked and unchecked todo items
    
    Issue: main

--- src/components/ListTodo.tsx
+++ src/components/ListTodo.tsx
@@ -1,37 +1,41 @@
-import {FC, useState} from "react";
+import {FC} from "react";
 import {ITodo} from "../App.tsx";
-import {IconCheck} from "../assets/images";
+import {IconCheck, IconCross} from "../assets/images";
 import {useThemeStore} from "../store/themeStore.tsx";
 
 type TodoListProps = {
     todos: ITodo[];
-}
+    onDeleteTodo: (id: number) => void;
+    onCheckTodo: (id: number) => void;
+};
 
-export const ListTodo: FC<TodoListProps> = ({ todos }) => {
-    const {themeProps, checkIcon} = useThemeStore()
-    const [check, setCheck] = useState(false);
-    const checkedBg = ' bg-gradient-to-br from-check-bg-start to-check-bg-end '
-    const handleChange = () => {
-        setCheck(!check);
-    };
+export const ListTodo: FC<TodoListProps> = ({  todos, onDeleteTodo, onCheckTodo  }) => {
+    const {themeProps, checkIcon} = useThemeStore();
 
     return (
-        <>
-            <div className={`${themeProps.secondaryBg} rounded-xl`}>
-                <ul>
-                    {todos.map(todo => (
-                        <div className={` h-12 flex flex-row items-center pl-5`}>
+        <div className={`${themeProps.secondaryBg} rounded-xl`}>
+            <ul>
+                {todos.map(todo => (
+                    <>
+                    <div key={todo.id} className="h-12 flex flex-row items-center justify-between pl-5">
+                        <div className={'flex flex-row items-center'}>
                             <button
-                                onClick={handleChange}
-                                className={`${check ? checkedBg : ''} flex justify-center items-center h-6 w-6 rounded-2xl`}>
-                                {check ? <img src={IconCheck} alt="Checked" /> : <img src={checkIcon} className={'size-6'}/>}
+                                onClick={() => onCheckTodo(todo.id)}
+                                className={`${todo.checked ? 'bg-gradient-to-br from-check-bg-start to-check-bg-end' : ''} h-6 w-6 rounded-2xl flex justify-center items-center`}>
+                                {todo.checked ? <img src={IconCheck} alt="Checked" /> : <img src={checkIcon} alt="Unchecked" className='w-6 h-6' />}
                             </button>
-                            <li key={todo.id} className={'ml-3'}>{todo.text}</li>
-                            <hr className={`${themeProps.contentColor}`}/>
+                            <li className={`${todo.checked ? 'line-through text-light-theme-dark-grayish-blue opacity-50' : ''} ml-3`}>
+                                {todo.text}
+                            </li>
                         </div>
-                    ))}
-                </ul>
-            </div>
-        </>
+                        <button onClick={() => onDeleteTodo(todo.id)}>
+                            <img src={IconCross} alt='Close todotask' className={'pr-7'} />
+                        </button>
+                    </div>
+                    <hr/>
+                    </>
+                ))}
+            </ul>
+        </div>
     );
 };
