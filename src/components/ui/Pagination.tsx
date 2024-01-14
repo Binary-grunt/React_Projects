37cf63a469052cefdcfe71ac03b4995af32730fe
Celare <119490047+C-elare@users.noreply.github.com>
2024-01-15 00:17:15 +0100

    feat(ui): Add Pagination component (main)
    - Add Pagination component to src/components/ui/Pagination.tsx
    - Implement onShowAll, onShowActive, and onShowCompleted functions
    - Style the component using themeProps
    - Render three buttons for All, Active, and Completed options

---
+++ src/components/ui/Pagination.tsx
@@ -0,0 +1,23 @@
+import {FC} from "react";
+import {useThemeStore} from "../../store/themeStore.tsx";
+
+
+type PaginationProps = {
+    onShowAll: () => void;
+    onShowActive: () => void;
+    onShowCompleted: () => void;
+}
+
+export const Pagination:FC<PaginationProps> = ({onShowAll, onShowActive, onShowCompleted}) => {
+    const {themeProps} = useThemeStore();
+    return (
+        <>
+            <div className={`${themeProps.secondaryText} ${themeProps.secondaryBg} 
+            h-16 flex flex-row items-center rounded-md justify-around `}>
+                <button onClick={onShowAll}>All</button>
+                <button onClick={onShowActive}>Active</button>
+                <button onClick={onShowCompleted}>Completed</button>
+            </div>
+        </>
+    );
+};
