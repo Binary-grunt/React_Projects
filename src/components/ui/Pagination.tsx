369ee74ce90f84ee8a22f440120b12d24c4b0d9c
Celare <119490047+C-elare@users.noreply.github.com>
2024-01-15 04:06:04 +0100

    feat(ui): update Pagination component styles (main)
    - Updated Pagination component styles to include a larger gap between buttons.
    - Changed the class name to include the "lg:gap-14" modifier.
    - Added a comment to explain the purpose of the code changes.

--- src/components/ui/Pagination.tsx
+++ src/components/ui/Pagination.tsx
@@ -13,7 +13,7 @@ export const Pagination:FC<PaginationProps> = ({onShowAll, onShowActive, onShowC
     return (
         <>
             <div className={`${themeProps.secondaryText} ${themeProps.secondaryBg} 
-            h-16 flex flex-row items-center rounded-md justify-around `}>
+            h-16 flex flex-row items-center rounded-md justify-around lg:gap-14`}>
                 <button onClick={onShowAll}>All</button>
                 <button onClick={onShowActive}>Active</button>
                 <button onClick={onShowCompleted}>Completed</button>
