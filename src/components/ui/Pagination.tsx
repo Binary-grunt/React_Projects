f7c8bd1d7b0079487852d2ec13aaa06a3851be11
Celare <119490047+C-elare@users.noreply.github.com>
2024-01-15 05:59:10 +0100

    feat(ui): update Pagination component styles (main)

--- src/components/ui/Pagination.tsx
+++ src/components/ui/Pagination.tsx
@@ -13,10 +13,10 @@ export const Pagination:FC<PaginationProps> = ({onShowAll, onShowActive, onShowC
     return (
         <>
             <div className={`${themeProps.secondaryText} ${themeProps.secondaryBg} 
-            h-16 flex flex-row items-center rounded-md justify-around lg:gap-14`}>
-                <button onClick={onShowAll}>All</button>
-                <button onClick={onShowActive}>Active</button>
-                <button onClick={onShowCompleted}>Completed</button>
+            h-16 flex flex-row items-center rounded-md justify-center shadow-md gap-5 sm:gap-4 sm:shadow-none`}>
+                <button onClick={onShowAll} className={`${themeProps.hoverText} focus:text-bright-blue`}>All</button>
+                <button onClick={onShowActive} className={`${themeProps.hoverText} focus:text-bright-blue`}><p>Active</p></button>
+                <button onClick={onShowCompleted} className={`${themeProps.hoverText} focus:text-bright-blue`}>Completed</button>
             </div>
         </>
     );
