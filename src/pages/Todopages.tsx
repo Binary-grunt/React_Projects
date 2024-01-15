3f3ea71b8c437b2d3e3e70d5fe47a4d341a09530
Celare <119490047+C-elare@users.noreply.github.com>
2024-01-15 07:16:10 +0100

    feat(Todopages): Add to-do functionality and filters commentary (main)

--- src/pages/Todopages.tsx
+++ src/pages/Todopages.tsx
@@ -1,7 +1,8 @@
-import {useCallback, useState} from "react";
-import {AddTodoInput} from "../components/Todo/AddTodoInput.tsx";
-import {ListTodo} from "../components/Todo/ListTodo.tsx";
+import { useCallback, useState } from "react";
+import { AddTodoInput } from "../components/Todo/AddTodoInput.tsx";
+import { ListTodo } from "../components/Todo/ListTodo.tsx";
 
+// Defining the structure of a to-do item.
 export type ITodo = {
     id: number;
     text: string;
@@ -10,33 +11,30 @@ export type ITodo = {
 }
 
 export const Todopage = () => {
+    // State management for to-dos and unique identifier generation.
     const [todos, setTodos] = useState<ITodo[]>([]);
     const [nextId, setNextId] = useState(1);
 
+    // useCallback to optimize the addTodoHandler, preventing unnecessary re-renders.
     const addTodoHandler = useCallback((text: string) => {
         setTodos(prevTodos => [...prevTodos, { id: nextId, text, checked: false, filter: '' }]);
         setNextId(nextId + 1);
     }, [nextId]);
 
+    // Function to delete a specific to-do item.
     const deleteTodoHandler = (id: number) => setTodos(todos.filter(todo => todo.id !== id));
-    const clearCheckedHandle = () => setTodos(todos.filter( todo => !todo.checked));
+
+    // Function to clear checked to-do items.
+    const clearCheckedHandle = () => setTodos(todos.filter(todo => !todo.checked));
+
+    // Function to toggle the checked state of a to-do item.
     const checkTodoHandler = (id: number) => setTodos(todos.map(todo =>
         todo.id === id ? { ...todo, checked: !todo.checked } : todo ));
+
+    // Functions to filter to-dos based on their checked status.
     const showAllTodo = () => setTodos(todos.map(todo => ({ ...todo, filter: '' })));
-
-    const showNotChecked = () => {
-        const notChecked = todos.filter(todo => !todo.checked);
-        if(notChecked && notChecked.length >= 1) {
-            setTodos(todos.map(todo => ({...todo, filter: todo.checked ? 'hidden' : ''})));
-        }
-    };
-
-    const showChecked = () => {
-        const checked = todos.filter(todo => todo.checked);
-        if(checked && checked.length >= 1){
-            setTodos(todos.map(todo => ({ ...todo, filter: !todo.checked ? 'hidden' : '' })));
-        }
-    };
+    const showNotChecked = () => setTodos(todos.map(todo => ({...todo, filter: todo.checked ? 'hidden' : ''})));
+    const showChecked = () => setTodos(todos.map(todo => ({ ...todo, filter: !todo.checked ? 'hidden' : '' })));
 
     return (
         <>
