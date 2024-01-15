7f4fd3453be434fa5a9695a66c8f9ceec10ec110
Celare <119490047+C-elare@users.noreply.github.com>
2024-01-15 07:22:11 +0100

    feat(AddTodoInput): Add functionality to add a new to-do item (main)
    - Import React, CSS for styling, react-hook-form, and Zod for form validation, and necessary types.
    - Define type definition for the component's props.
    - Create functional component for adding a new to-do item.
    - Access theme properties and set up form with react-hook-form and Zod for validation.
    - Implement handler function to submit the form data.
    - Render container for the input form.
    - Render form element with submit handler.
    - Display the check icon.
    - Render input field for new to-do.
    - Display error message if input validation fails.

--- src/components/Todo/AddTodoInput.tsx
+++ src/components/Todo/AddTodoInput.tsx
@@ -1,20 +1,25 @@
+// Importing React, CSS for styling, react-hook-form and Zod for form validation, and necessary types.
 import {FC} from "react";
-import './../../css/AddTodoInput.css'
+import './../../css/AddTodoInput.css';
 import { useForm } from "react-hook-form";
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useThemeStore } from "../../store/themeStore.tsx";
 import {ITodoInput, TodoSchema} from "../../models/todoModel.ts";
 
+// TypeScript type definition for the component's props.
 type AddTodoFormProps = {
-    onAddTodo: (todoText: string) => void;
+    onAddTodo: (todoText: string) => void; // Function to add a new to-do.
 };
 
+// Functional component for adding a new to-do item.
 export const AddTodoInput:FC<AddTodoFormProps> = ({onAddTodo}) => {
+    // Accessing theme properties and setting up form with react-hook-form and Zod for validation.
     const { themeProps, checkIcon } = useThemeStore();
     const { register, handleSubmit, reset, formState: { errors } } = useForm<ITodoInput>({
         resolver: zodResolver(TodoSchema)
     });
 
+    // Handler function to submit the form data.
     const onSubmit = (data: ITodoInput) => {
         onAddTodo(data.newTodo);
         reset();
@@ -22,21 +27,23 @@ export const AddTodoInput:FC<AddTodoFormProps> = ({onAddTodo}) => {
 
     return (
         <>
-            <div  className={`${themeProps.secondaryBg} 
-            h-16 flex flex-row items-center rounded-md shadow-md min-width sm:h-16`}>
+            {/* Container for the input form */}
+            <div className={`${themeProps.secondaryBg} h-16 flex flex-row items-center rounded-md shadow-md min-width sm:h-16`}>
+                {/* Form element with submit handler */}
                 <form onSubmit={handleSubmit(onSubmit)} className='pl-5 flex flex-row items-center gap-1'>
+                    {/* Displaying the check icon */}
                     <img src={checkIcon} className='w-6 h-6'  alt='Check icon'/>
+                    {/* Input field for new to-do */}
                     <input
                         {...register('newTodo', { required: true })}
                         placeholder="Add a new task"
                         className={`ml-3 ${themeProps.secondaryBg} ${themeProps.primaryText}
-                        border-none w-96 h-12 sm:placeholder:text-lg focus:outline-none `}
+                    border-none w-36 h-12 sm:w-96 sm:placeholder:text-lg focus:outline-none `}
                     />
-                    {errors.newTodo && <p className={"error-message italic text-xs font-josefin text-bright-blue"}>{errors.newTodo.message}</p>}
-                    <button type="submit" className="ml-2"></button>
+                    {/* Error message display if input validation fails */}
+                    {errors.newTodo && <p className={"error-message italic text-xs font-josefin pr-2 text-bright-blue"}>{errors.newTodo.message}</p>}
                 </form>
             </div>
-
         </>
     );
 };
