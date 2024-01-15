a8abd7abe54161f497dd3c0c488ef018484a9bb0
Celare <119490047+C-elare@users.noreply.github.com>
2024-01-15 05:58:43 +0100

    feat(App): Update App.css import and refactor layout design (main)

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import './App.css'
+import './css/App.css'
 import {useThemeStore} from "./store/themeStore.tsx";
 import {Header} from "./components/Header.tsx";
 import {Todopage} from "./pages/Todopages.tsx";
@@ -9,12 +9,16 @@ export const App = () => {
 
   return (
         <>
-            <div className={`${themeProps.primaryBg} h-screen`}>
+            <div className={`${themeProps.primaryBg} h-screen overflow-auto will-change-scroll`}>
                 <div
                     style={backgroundStyle}
-                    className={`${themeProps.primaryBg} font-josefin bg-cover bg-center bg-no-repeat h-52 px-7`}>
-                    <Header/>
-                    <Todopage/>
+                    className={`${themeProps.primaryBg} font-josefin bg-cover bg-center bg-no-repeat h-52 px-7 sm:h-80`}>
+                    <div className={'center-content'}>
+                    <div className={'flex flex-col sm:min-w-max sm:max-w-xl'}>
+                        <Header/>
+                        <Todopage/>
+                    </div>
+                    </div>
                 </div>
             </div>
         </>
